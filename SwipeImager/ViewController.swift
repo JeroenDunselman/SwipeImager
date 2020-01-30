136a93601adf8c81e122abccf164b29c936a8797
Jeroen Dunselman <jeroduns@gmail.com>
2020-01-30 14:55:56 +0100

    Initial Commit

---
+++ SwipeImager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeImager
+//
+//  Created by Jeroen Dunselman on 30/01/2020.
+//  Copyright © 2020 Jeroen Dunselman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
